f4698a1ba61c4c52461d9d45a9161230f033d8a4
Alan Nguyen <tuan8101@gmail.com>
2020-03-29 22:28:28 -0600

    Add inRange method

--- main.js
+++ main.js
@@ -3,5 +3,18 @@ const _ = {
     let lowerClampedValue = Math.max(lower, number);
     let clampedValue = Math.min(lowerClampedValue, upper);
     return clampedValue;
+  },
+  inRange(number, start, end) {
+    if (typeof end === 'undefined') {
+      end = start;
+      start = 0;
+    }
+    if (start > end) {
+      let temp = end;
+      end = start;
+      start = temp;
+    }
+    let isInRange = start <= number && number < end;
+    return isInRange;
   }
 };
