8f85cea05c42403263de4254ce5d94f98ab3a575
Alan Nguyen <tuan8101@gmail.com>
2020-03-29 22:28:13 -0600

    Add clamp method

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-const _ = {};
+const _ = {
+  clamp(number, lower, upper) {
+    let lowerClampedValue = Math.max(lower, number);
+    let clampedValue = Math.min(lowerClampedValue, upper);
+    return clampedValue;
+  }
+};
